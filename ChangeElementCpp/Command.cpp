78c3df4553bc10e8c7ffed620c4541f715ed6df4
halukuzuner <46990292+halukuzuner@users.noreply.github.com>
2022-10-05 23:54:41 +0300

    Add files via upload

---
+++ ChangeElementCpp/Command.cpp
@@ -0,0 +1,60 @@
+#include "pch.h"
+
+#include "Command.h"
+#include "WindowHandler.h"
+#include "MainForm.h"
+#include "EventHandler.h"
+#include "GlobalVariables.h"
+
+using namespace System;
+using namespace System::Collections::Generic;
+using namespace System::Text;
+using namespace System::Windows::Forms;
+using namespace System::Reflection;
+using namespace System::ComponentModel;
+using namespace System::Globalization;
+using namespace System::Resources;
+using namespace System::Threading;
+
+using namespace Autodesk::Revit;
+using namespace Autodesk::Revit::DB;
+using namespace Autodesk::Revit::UI;
+
+namespace ChangeElementCpp
+{
+	Result Command::Execute(ExternalCommandData^ commandData, String^% message, Autodesk::Revit::DB::ElementSet^ elements)
+	{	
+		//	To handle main Revit window.
+		if (nullptr == Command::_hWndRevit)
+		{
+			Process^ process = Process::GetCurrentProcess();
+
+			IntPtr h = process->MainWindowHandle;
+			Command::_hWndRevit = gcnew WindowHandle(h);
+		}
+
+		try
+		{
+			// A new handler to handle request posting by the dialog
+			iexeventhandler^ handler = gcnew iexeventhandler();
+
+			// External Event for the dialog to use (to post requests)
+			ExternalEvent^ exEvent = ExternalEvent::Create(handler);
+			
+			if (mainForm == nullptr || mainForm->IsDisposed)
+			{
+				// We give the objects to the new dialog;
+				// The dialog becomes the owner responsible for disposing them, eventually.
+				// process data from Revit and show dialog
+				mainForm = gcnew MainForm(commandData, exEvent, handler);
+				mainForm->Show(Command::_hWndRevit);
+			}
+		}
+		catch (Exception^ ex)
+		{
+			message = ex->Message;
+			return Autodesk::Revit::UI::Result::Failed;
+		}
+		return Autodesk::Revit::UI::Result::Cancelled;
+	}
+}
